15c5a0a4490840700d649678197703c04d1188b3
lipengwei <1970586140@qq.com>
2025-08-21 02:09:37 +0800

    <optimize>
    <div>去除多余的分包</div>
    </optimize>

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,4 +1,4 @@
-import { getBetween } from "./validate";
+// import { getBetween } from "./validate";
 export function createNameThroughPath(path: string): string {
   let chunkName = "";
   // 依赖包分包
@@ -9,16 +9,16 @@ export function createNameThroughPath(path: string): string {
     return `vendor-${chunkName}`;
   }
   // src下views页面分包
-  if (path.includes("src/views/")) {
-    chunkName = path.split("/")[6];
-    if (chunkName.includes("index")) {
-      chunkName = path.split("/")[5];
-    }
-    return `views-${chunkName}`;
-  }
+  // if (path.includes("src/views/")) {
+  //   chunkName = path.split("/")[6];
+  //   if (chunkName.includes("index")) {
+  //     chunkName = path.split("/")[5];
+  //   }
+  //   return `views-${chunkName}`;
+  // }
   // src下除views之外的目录分包
-  if (path.includes("src/")) {
-    return getBetween(path, "src/", "/");
-  }
+  // if (path.includes("src/")) {
+  //   return getBetween(path, "src/", "/");
+  // }
   return chunkName;
 }
