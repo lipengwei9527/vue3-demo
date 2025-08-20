56abb57224285751ef036e48c1ebd05cff91b3b4
lipengwei <1970586140@qq.com>
2025-08-21 06:40:20 +0800

    Updates

---
+++ js/i18n.Df1vfhZd.js
@@ -0,0 +1,1 @@
+import{c as a}from"./vendor-vue-i18n.B9bHR1D8.js";const l={language:"english"},e={language:"中文"},o={en:{...l},zh:{...e}},n=a({locale:localStorage.getItem("local")||"zh",legacy:!1,globalInjection:!0,fallbackLocale:"zh",messages:o});export{n as i};
---
+++ js/imgs.B0wbXNrQ.js
@@ -0,0 +1,1 @@
+const g="/png/login_background.C4JTMCLd.png";export{g as l};
