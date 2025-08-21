2d3e8c7bc5f86f26caaf898c8a73b5cd0a768f39
lipengwei <1970586140@qq.com>
2025-08-21 15:41:40 +0800

    Updates

--- js/i18n.rmUZ85Lh.js
+++
@@ -1,2 +0,0 @@
-import{c as a}from"./vendor-vue-i18n.WpppbJaZ.js";const l={language:"english"},e={language:"中文"},o={en:{...l},zh:{...e}},n=a({locale:localStorage.getItem("local")||"zh",legacy:!1,globalInjection:!0,fallbackLocale:"zh",messages:o});export{n as i};
-//# sourceMappingURL=i18n.rmUZ85Lh.js.map
--- js/views-BackLayout.vue.acm9ljYg.js
+++
@@ -1,2 +0,0 @@
-import"./views-BackLayout.vue_vue_type_script_setup_true_name_BackLayout_lang.ts.My5Vu2NJ.js";
-//# sourceMappingURL=views-BackLayout.vue.acm9ljYg.js.map
